3a07cb8a8b863096906aa5c7e7413c69dba81da5
ArnoldBF <abf1997.ab@gmail.com>
2024-01-28 14:05:07 -0400

    integracion de bcryptjs

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,9 @@
-const { response,request } = require("express"); // importamos response de express
+const { response, request } = require("express"); // importamos response de express
+const Usuario = require("../models/user"); //importamos el modelo de usuario
+const bcryptjs = require("bcryptjs"); //importamos bcryptjs
 
-const usuariosGet = (req=request, res = response) => {
-
-    const {q,nombre='no name',apikey,page="1",limit}=req.query;
+const usuariosGet = (req = request, res = response) => {
+  const { q, nombre = "no name", apikey, page = "1", limit } = req.query;
   res.json({
     msg: "get API - controlador",
     q,
@@ -13,32 +14,43 @@ const usuariosGet = (req=request, res = response) => {
   });
 };
 
-const usuariosPut = (req=request, res = response) => {
-    const {id}=req.params;
+const usuariosPut = (req = request, res = response) => {
+  const { id } = req.params;
   res.json({
     msg: "put API - controlador",
-    id
-  });
-};
-
-const usuariosPost = (req=request, res = response) => {
-  const {nombre,ci,id,edad} = req.body;
-  res.status(201).json({
-    msg: "post API - controlador",
-    nombre,
-    ci,
     id,
-    edad
   });
 };
 
-const usuariosDelete = (req=request, res = response) => {
+const usuariosPost = async (req = request, res = response) => {
+  const { nombre, correo, password, rol } = req.body;
+  const usuario = new Usuario({ nombre, correo, password, rol });
+
+  //verificar si el correo existe
+  const existeEmail = await Usuario.findOne({ correo: correo });
+
+  if (existeEmail) {
+    return res.status(400).json({
+      msg: "El correo ya esta registrado",
+    });
+  }
+  //encriptar la contraseña
+  const salt = bcryptjs.genSaltSync(); //generamos el salt
+  usuario.password = bcryptjs.hashSync(password, salt); //encriptamos la contraseña
+  //guardar en db
+  await usuario.save();
+  res.status(201).json({
+    usuario,
+  });
+};
+
+const usuariosDelete = (req = request, res = response) => {
   res.json({
     msg: "delete API - controlador",
   });
 };
 
-const usuariosPatch = (req=request, res = response) => {
+const usuariosPatch = (req = request, res = response) => {
   res.json({
     msg: "patch API - controlador",
   });
